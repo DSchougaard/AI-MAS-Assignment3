2776682b4927f993f37842c99c81518e9285b23c
Reindahl <Reindahl1@hotmail.com>
2015-03-17 18:33:09 +0100

    typo fixs

--- environment/client/SearchClient.java
+++ environment/client/SearchClient.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -196,9 +197,11 @@ public class SearchClient {
 	}
 	
 	private static List< Agent > agents = new ArrayList< Agent >();
+	
+	
 	public static void main( String[] args ) throws Exception {
 		BufferedReader serverMessages = new BufferedReader( new InputStreamReader( System.in ) );
-
+agents.add(new Agent('0', "red"));
 		// Use stderr to print to console
 		System.err.println( "SearchClient initializing. I am sending this using the error output stream." );
 
@@ -221,7 +224,6 @@ public class SearchClient {
 		for (Agent agent : agents) {
 			solutions.add(client.Search( strategy , agent ));
 		}
-
 //		merge solutions
 		String[] solution;
 		
@@ -233,25 +235,26 @@ public class SearchClient {
 			
 		}
 		solution = new String[size];
-		for (int i = 0; i < solutions.size(); i++) {
+		for (int i = 0; i < solution.length; i++) {
 			solution[i]="[";
 		}
 		int j=0;
+		
 		for (int i = 0; i < solutions.size(); i++) {
-			for (int k = 0; i < solutions.get(i).size(); i++) {
+			for (int k = 0; k < solutions.get(i).size(); k++) {
 				
-				if(k!=solutions.get(i).size()){
-					solution[i]+=solutions.get(i).get(k).action.toString()+",";
+				if(i!=solutions.size()-1){
+					solution[k]+=solutions.get(i).get(k).action.toString()+",";
 				}else{
-					solution[i]+=solutions.get(i).get(k).action.toString();
+					solution[k]+=solutions.get(i).get(k).action.toString();
 				}
 			}
 		}
-		for (int i = 0; i < solutions.size(); i++) {
+		for (int i = 0; i < solution.length; i++) {
 			solution[i]+="]";
 		}
 		
-		
+		System.err.println(Arrays.toString(solution));
 		if ( solution == null ) {
 			System.err.println( "Unable to solve level" );
 			System.exit( 0 );
