d4d8ce03869cfb11e55ce442e87a06f9a6e85b0c
Martin Vestager Ottesen <martin_v_ottesen@hotmail.com>
2015-04-21 17:08:50 +0200

    repaired my fuckup

--- environment/start/SearchMAsimple3.bat
+++ environment/start/SearchMAsimple3.bat
@@ -2,7 +2,7 @@ cd %~dp0
 cd ..
 del /s /q /f *.class
 cls
-javac -cp . client/MAsimple3.java
-java -jar server.jar -l levels/MAchallenge.lvl -g 200 -c "java  -Xmx2048m client.SearchClient"
+javac -cp . client/SearchClient.java
+java -jar server.jar -l levels/MAsimple3.lvl -g 200 -c "java  -Xmx2048m client.SearchClient"
 
 pause
