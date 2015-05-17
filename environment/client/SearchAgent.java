3c8df2e7f055960ec1c1fe6f09b0837ed78bc98b
Reindahl <Reindahl1@hotmail.com>
2015-05-17 12:50:54 +0200

    killed finit

--- environment/client/SearchAgent.java
+++ environment/client/SearchAgent.java
@@ -1,7 +1,6 @@
 package client;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Deque;
 import java.util.LinkedList;
 
 import client.SearchClient.Memory;
@@ -12,13 +11,11 @@ import client.node.Color;
 import client.node.GoalState;
 import client.node.GoalState.GlobalGoldGoalState;
 import client.node.Node;
-import client.node.storage.Base;
 import client.node.storage.ExpansionStatus;
 import client.node.storage.Goal;
 import client.node.storage.LogicalAgent;
 import client.node.storage.SearchResult;
 // Includes necessary for detecting running in circles
-import client.utils.FiniteQueue;
 
 
 public class SearchAgent{
