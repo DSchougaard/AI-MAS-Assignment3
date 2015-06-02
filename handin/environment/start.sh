8196d6b61cc469cd586f567947bc6ed164b24e45
Daniel Schougaard <danielschougaard@gmail.com>
2015-06-02 23:32:05 +0200

    Removals

--- handin/environment/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-javac -cp . client/GuiClient.java
-java -jar server.jar -l levels/SACrunch.lvl -g 200 -c "java  -Xmx2048m client.SearchClient -dm FloydWarshallDistanceMap"
--- handin/environment/start_random.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-javac -cp . client/RandomWalkClient.java
-java -jar server.jar -l levels/SAtest.lvl -g 200 -c "java  -Xmx2048m client.RandomWalkClient"
